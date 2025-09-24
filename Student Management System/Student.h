07c7bd418d7cd7e6ac11dc44dcb4fd041bf2c6ed
CodingZap <help@codingzap.com>
2025-09-24 10:10:57 +0530

    Create Student.h

---
+++ Student Management System/Student.h
@@ -0,0 +1,12 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <string>
+using namespace std;
+
+// Structure to hold student information
+struct Student {
+    int id;
+    string name;
+    string course;
+    float gpa;
+};
