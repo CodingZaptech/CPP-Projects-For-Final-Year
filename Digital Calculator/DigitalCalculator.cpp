55284b0d39dde003a937fcd778e2e75869a5b570
CodingZap <help@codingzap.com>
2025-09-24 13:50:35 +0530

    Update DigitalCalculator.cpp

--- Digital Calculator/DigitalCalculator.cpp
+++ Digital Calculator/DigitalCalculator.cpp
@@ -1,53 +1,123 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
+#include <QApplication>
+#include <QWidget>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QString>
+#include <QDebug>
 
-int main() {
-    double num1, num2, result;
-    char op;
+class DigitalCalculatorGUI : public QWidget {
+    Q_OBJECT
 
-    // Display calculator interface
-    cout << "===== Digital Calculator =====" << endl;
-    cout << "Enter first number: ";
-    cin >> num1;
+public:
+    DigitalCalculatorGUI(QWidget *parent = nullptr) : QWidget(parent) {
+        setWindowTitle("Digital Calculator");
 
-    cout << "Enter operator (+, -, *, /, ^, %): ";
-    cin >> op;
+        display = new QLineEdit();
+        display->setReadOnly(true);
+        display->setAlignment(Qt::AlignRight);
+        display->setFixedHeight(40);
 
-    cout << "Enter second number: ";
-    cin >> num2;
+        // Create buttons
+        QString buttons[4][4] = {
+            {"7","8","9","/"},
+            {"4","5","6","*"},
+            {"1","2","3","-"},
+            {"0",".","=","+"}
+        };
 
-    // Perform calculation based on operator
-    switch(op) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0)
-                result = num1 / num2;
-            else {
-                cout << "Error: Division by zero!" << endl;
-                return 1;
+        QGridLayout *grid = new QGridLayout();
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                QPushButton *btn = new QPushButton(buttons[i][j]);
+                btn->setFixedSize(60,60);
+                grid->addWidget(btn,i,j);
+                connect(btn,&QPushButton::clicked,this,[=](){ onButtonClicked(buttons[i][j]); });
             }
-            break;
-        case '^':
-            result = pow(num1, num2);
-            break;
-        case '%':
-            result = fmod(num1, num2);
-            break;
-        default:
-            cout << "Invalid operator!" << endl;
-            return 1;
+        }
+
+        // Clear button
+        QPushButton *clearBtn = new QPushButton("C");
+        clearBtn->setFixedHeight(40);
+        connect(clearBtn, &QPushButton::clicked, this, &DigitalCalculatorGUI::clearDisplay);
+
+        QVBoxLayout *mainLayout = new QVBoxLayout();
+        mainLayout->addWidget(display);
+        mainLayout->addLayout(grid);
+        mainLayout->addWidget(clearBtn);
+
+        setLayout(mainLayout);
     }
 
-    cout << "Result: " << result << endl;
+private slots:
+    void onButtonClicked(QString text){
+        if(text=="="){
+            calculateResult();
+        } else {
+            currentInput += text;
+            display->setText(currentInput);
+        }
+    }
 
-    return 0;
+    void clearDisplay(){
+        currentInput = "";
+        display->setText("");
+        operand1 = operand2 = 0;
+        operation = "";
+    }
+
+private:
+    QLineEdit *display;
+    QString currentInput;
+    double operand1=0, operand2=0;
+    QString operation="";
+
+    void calculateResult(){
+        QString temp = currentInput;
+        QChar opChar;
+        int opIndex = -1;
+
+        // Find operator (+,-,*,/,^,%)
+        for(int i=0;i<temp.length();i++){
+            if(temp[i]=='+' || temp[i]=='-' || temp[i]=='*' || temp[i]=='/' || temp[i]=='^' || temp[i]=='%'){
+                opChar = temp[i];
+                opIndex = i;
+                break;
+            }
+        }
+
+        if(opIndex==-1){
+            display->setText("Invalid input!");
+            return;
+        }
+
+        operand1 = temp.left(opIndex).toDouble();
+        operand2 = temp.mid(opIndex+1).toDouble();
+        double result = 0;
+
+        if(opChar=='+') result = operand1 + operand2;
+        else if(opChar=='-') result = operand1 - operand2;
+        else if(opChar=='*') result = operand1 * operand2;
+        else if(opChar=='/'){
+            if(operand2==0){ display->setText("Error: Division by zero!"); return; }
+            result = operand1 / operand2;
+        }
+        else if(opChar=='^') result = pow(operand1, operand2);
+        else if(opChar=='%') result = fmod(operand1, operand2);
+
+        display->setText(QString::number(result));
+        currentInput = QString::number(result);
+    }
+};
+
+#include "main.moc"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    DigitalCalculatorGUI calculator;
+    calculator.show();
+    return app.exec();
 }
