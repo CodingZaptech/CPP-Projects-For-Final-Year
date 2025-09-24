5845de1304dac6d886726518fe58e91a9c52607e
CodingZap <help@codingzap.com>
2025-09-24 13:29:51 +0530

    Create Car.h

---
+++ Car Rental Application/Car.h
@@ -0,0 +1,12 @@
+#ifndef CAR_H
+#define CAR_H
+#include <string>
+using namespace std;
+
+// Structure for car details
+struct Car {
+    int id;
+    string model;
+    bool available; // true if car is available for rent
+};
+
