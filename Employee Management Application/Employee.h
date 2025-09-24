603e61315ea91041d22a23b119765b585c721069
CodingZap <help@codingzap.com>
2025-09-24 13:34:27 +0530

    Create Employee.h

---
+++ Employee Management Application/Employee.h
@@ -0,0 +1,12 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+#include <string>
+using namespace std;
+
+// Structure for employee information
+struct Employee {
+    int id;
+    string name;
+    string position;
+};
+
