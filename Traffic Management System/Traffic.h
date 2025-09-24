3919237cad5e973689ea9ce77bdbc8cdfa246b3a
CodingZap <help@codingzap.com>
2025-09-24 10:18:19 +0530

    Create Traffic.h

---
+++ Traffic Management System/Traffic.h
@@ -0,0 +1,12 @@
+#ifndef TRAFFIC_H
+#define TRAFFIC_H
+#include <string>
+using namespace std;
+
+// Structure for vehicle information
+struct Vehicle {
+    int id;
+    string type;
+    string owner;
+};
+
