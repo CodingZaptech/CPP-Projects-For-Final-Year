f611a2c3fce948010ff828c4b98bd148c471dafc
CodingZap <help@codingzap.com>
2025-09-24 13:32:09 +0530

    Create Dating.h

---
+++ Dating Application/Dating.h
@@ -0,0 +1,12 @@
+#ifndef DATING_H
+#define DATING_H
+#include <string>
+using namespace std;
+
+// Structure for user profile
+struct Profile {
+    string name;
+    int age;
+    string interest;
+};
+
