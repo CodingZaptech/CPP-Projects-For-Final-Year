edc3a9482c9f56a022f86df837859e592acec039
CodingZap <help@codingzap.com>
2025-09-24 13:27:36 +0530

    Create Security.h

---
+++ Security System/Security.h
@@ -0,0 +1,11 @@
+#ifndef SECURITY_H
+#define SECURITY_H
+#include <string>
+using namespace std;
+
+// Structure for user credentials
+struct User {
+    string username;
+    string password;
+};
+
