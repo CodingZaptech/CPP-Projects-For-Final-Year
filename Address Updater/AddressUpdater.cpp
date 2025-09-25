924766bc81c496742b4af9634b7e54ab375af0a6
CodingZap <help@codingzap.com>
2025-09-25 08:41:51 +0530

    Update AddressUpdater.cpp

--- Address Updater/AddressUpdater.cpp
+++ Address Updater/AddressUpdater.cpp
@@ -1,52 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-
-struct Person {
-    string name;
-    string address;
-};
+#include "AddressUpdater.h"
+#include "Backend.cpp"
 
 int main() {
-    int n;
-    cout << "===== Address Updater =====" << endl;
-    cout << "Enter number of persons: ";
-    cin >> n;
-    cin.ignore(); // Clear newline
+    AddressUpdater updater;
+    int choice;
 
-    vector<Person> people(n);
+    do {
+        cout << "\n--- Address Updater ---\n";
+        cout << "1. Set Address\n2. Show Address\n3. Exit\nChoice: ";
+        cin >> choice;
 
-    // Input person details
-    for(int i = 0; i < n; i++) {
-        cout << "Enter name of person " << i+1 << ": ";
-        getline(cin, people[i].name);
-        cout << "Enter address of person " << i+1 << ": ";
-        getline(cin, people[i].address);
-    }
+        handleAddressChoice(updater, choice);
 
-    // Update address
-    string searchName;
-    cout << "\nEnter the name of person to update address: ";
-    getline(cin, searchName);
-
-    bool found = false;
-    for(auto &person : people) {
-        if(person.name == searchName) {
-            cout << "Enter new address: ";
-            getline(cin, person.address);
-            found = true;
-            break;
-        }
-    }
-
-    if(!found) cout << "Person not found!" << endl;
-
-    // Display updated list
-    cout << "\n===== Updated List =====" << endl;
-    for(auto &person : people) {
-        cout << "Name: " << person.name << ", Address: " << person.address << endl;
-    }
+    } while (choice != 3);
 
     return 0;
 }
