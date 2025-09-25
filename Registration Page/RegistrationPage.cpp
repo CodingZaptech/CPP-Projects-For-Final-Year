29174e14f2a0d5c348323923dab0f65158e0ac27
CodingZap <help@codingzap.com>
2025-09-25 08:43:31 +0530

    Update RegistrationPage.cpp

--- Registration Page/RegistrationPage.cpp
+++ Registration Page/RegistrationPage.cpp
@@ -1,36 +1,18 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-
-struct User {
-    string name;
-    int age;
-    string email;
-};
+#include "Registration.h"
+#include "Backend.cpp"
 
 int main() {
-    User user;
-    cout << "===== Registration Page =====" << endl;
+    Registration reg;
+    int choice;
 
-    // Input user details
-    cout << "Enter Name: ";
-    getline(cin, user.name);
-    cout << "Enter Age: ";
-    cin >> user.age;
-    cin.ignore();
-    cout << "Enter Email: ";
-    getline(cin, user.email);
+    do {
+        cout << "\n--- Registration Page ---\n";
+        cout << "1. Register User\n2. Show User\n3. Exit\nChoice: ";
+        cin >> choice;
 
-    // Save to file
-    ofstream file("users.txt", ios::app);
-    if(file.is_open()) {
-        file << "Name: " << user.name << ", Age: " << user.age << ", Email: " << user.email << endl;
-        file.close();
-        cout << "Registration Successful!" << endl;
-    } else {
-        cout << "Error opening file!" << endl;
-    }
+        handleRegistrationChoice(reg, choice);
+
+    } while (choice != 3);
 
     return 0;
 }
